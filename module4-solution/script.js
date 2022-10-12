df09aab254d497fdd7183c215fa117d1be65d720
Giviko92 <114116030+Giviko92@users.noreply.github.com>
2022-10-12 07:17:39 +0400

    Add files via upload

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,1 +1,23 @@
+var names=new Array();
+names[0]="Giviko"
+names[1]="John"
+names[2]="Jason"
+names[3]="jimmy"
+names[4]="jonathan"
+names[5]="michael"
+names[6]="alex"
+names[7]="rodrigo"
+names[8]="gustavo"
+names[9]="Guga"
+
+
+for (var i = 0; i < names.length; i++){
+	if (names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+       console.log("Goodbye" + names[i])
+	}
+	else{
+		console.log("Hello" + names [i])
+	}
+	
+} 
 
