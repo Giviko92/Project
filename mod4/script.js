8b2ca87e61098246b38b3f40e2c655647f0c88ea
Giviko92 <114116030+Giviko92@users.noreply.github.com>
2022-10-12 05:38:08 +0400

    Add files via upload

---
+++ mod4/script.js
@@ -0,0 +1,16 @@
+var names=new Array();
+names[0]="Giviko"
+names[1]="John"
+names[2]="Jason"
+names[3]="jimmy"
+names[4]="jonathan"
+names[5]="michael"
+names[6]="alex"
+names[7]="rodrigo"
+names[8]="gustavo"
+names[9]="Guga"
+
+for (var i =0; i < names.length; i**){
+	
+	console.log(names[i]);
+} 
